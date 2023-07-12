1e6f40e3e3374a51e65d4aa1d40388da41afec23
User <svinogr@hotmail.com>
2023-07-12 09:50:30 +0300

    added fix fab and items

--- mycompose/src/main/java/info/upump/mycompose/ui/screens/myworkouts/MyCycleScreen.kt
+++ mycompose/src/main/java/info/upump/mycompose/ui/screens/myworkouts/MyCycleScreen.kt
@@ -2,13 +2,21 @@ package info.upump.mycompose.ui.screens.myworkouts
 
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -20,10 +28,15 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -36,43 +49,59 @@ import java.util.Date
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun MyCycleScreen(navHostController: NavHostController, paddingValues: PaddingValues) {
-    val listState = rememberLazyListState()
-     val fabVisibility by derivedStateOf {
-          listState.firstVisibleItemIndex == 0
-      }
 
-    Box() {
-        LazyColumn(modifier = Modifier
-            .padding(top = paddingValues.calculateTopPadding(), bottom = paddingValues.calculateBottomPadding()),
-        state = listState) {
-            item {
-                for (i in 1..20) {
-                    val cycle = Cycle(title = "pipec $i")
-                    cycle.image = "uk1"
-                    cycle.startDate = Date()
-                    cycle.finishDate = Date()
+
+    Box(
+        Modifier
+            .background(Color.Red)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(paddingValues)
+    ) {
+        val listState = rememberLazyListState()
+
+        LazyColumn(
+            state = listState
+        ) {
+            for (i in 1..20) {
+                val cycle = Cycle(title = "pipec $i")
+                cycle.image = "uk1"
+                cycle.startDate = Date()
+                cycle.finishDate = Date()
+                item {
+
                     CycleItemCard(cycle = cycle)
                 }
             }
         }
 
-        var density = LocalDensity.current
-        AnimatedVisibility(
+        val fabVisibility by remember {
+
+            derivedStateOf {
+                listState.firstVisibleItemIndex > 0
+            }
+        }
+        Log.d("TAG", "$fabVisibility ${listState.firstVisibleItemIndex}")
+
+        val density = LocalDensity.current
+        AnimatedVisibility(modifier = Modifier
+            .padding(end = 16.dp, bottom = 16.dp)
+            .align(Alignment.BottomEnd),
             visible = fabVisibility,
             enter = slideInVertically {
+                // Slide in from 40 dp from the top.
                 with(density) { 40.dp.roundToPx() }
-            } + fadeIn(),
-            exit = fadeOut(
-                animationSpec = keyframes {
-                    this.durationMillis = 120
-                }
-            )
+            } + expandVertically(
+                // Expand from the top.
+                expandFrom = Alignment.Top
+            ) + fadeIn(
+                // Fade in with the initial alpha of 0.3f.
+                initialAlpha = 0.3f
+            ),
+            exit = slideOutVertically() + shrinkVertically() + fadeOut()
         ) {
             FloatingActionButton(
                 onClick = {},
-                Modifier
-                    .padding(end = 16.dp, bottom = paddingValues.calculateBottomPadding() + 16.dp)
-                    .align(Alignment.BottomEnd),
                 shape = CircleShape,
             ) {
                 Icon(
